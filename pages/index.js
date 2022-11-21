841ce9b0cf26161db47cea74fde3a9a336950c02
ahsenmirza1 <ahsen.s.mirza@gmail.com>
2022-11-21 17:31:48 +0500

    refactor"

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,6 @@
 import Head from 'next/head'
 import Image from 'next/image'
+import Link from 'next/link'
 import styles from '../styles/Home.module.css'
 
 export default function Home() {
@@ -12,10 +13,11 @@ export default function Home() {
       </Head>
       <nav className={styles.mainNav}>
         <ul>
-          <li>Home</li>
-          <li>About</li>
-          <li>Blog</li>
-          <li>Contact</li>
+          <li> <Link href='/'>Home</Link></li>
+          <li> <Link href='/about'>About</Link></li>
+          <li> <Link href='/blog'>Blog</Link></li>
+          <li> <Link href='/contact'>Contact</Link></li>
+         
         </ul>
       </nav>
 
